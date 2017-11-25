627f24545f7dbd99f2bf5959ff99e819da0ac98d
Tim Haskins <tijohask@iupui.edu>
2017-11-25 14:39:47 -0500

    Final touch-ups to friends route before pushing.

--- app/routes/friends.js
+++ app/routes/friends.js
@@ -4,10 +4,10 @@ export default Route.extend({
   model(params) {
     return this.get('store').query('user', { friends: true }).then((response) => {
       response.forEach(function(userId) {
-        if (userId == '__v') {
-          // do nothing
+        if (userId.user1 == ${this.get('gatekeeper.currentUser.id')}) {
+          console.log(userId.user2);
         } else {
-          // get image?
+          console.log(userId.user1);
         }
       });
     });
