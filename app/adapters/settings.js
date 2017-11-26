5d5891dffd1e3e191854a1164ebc453dce6fafbe
Josiah Campbell <jbcampbe@iupui.edu>
2017-11-26 16:07:31 -0500

    working on the settings page

---
+++ app/adapters/settings.js
@@ -0,0 +1,22 @@
+import DS from 'ember-data';
+import { computed } from '@ember/object';
+import { inject as service } from '@ember/service';
+
+
+let { RESTAdapter } = DS;
+
+export default RESTAdapter.extend({
+  gatekeeper: service(),
+
+  host: 'http://localhost:5000',
+  namespace: 'v1',
+
+  headers: computed('gatekeeper.accessToken', function() {
+    let accessToken = this.get('gatekeeper.accessToken.access_token');
+    return { Authorization: `Bearer ${accessToken}` };
+  }),
+
+  buildURL(modelName, id, snapshot, requestType, query) {
+    return `${this.get('host')}/${this.get('namespace')}/user/${this.get('gatekeeper.currentUser.id')}/criteria`;
+  }
+});
