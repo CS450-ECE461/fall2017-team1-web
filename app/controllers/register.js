4b2bf146fae5cba603b79e5142baf5e305204771
Josiah Campbell <jbcampbe@iupui.edu>
2017-12-12 16:32:38 -0500

    partway through getting the registration working with the real backend. code is ugly right now

--- app/controllers/register.js
+++ app/controllers/register.js
@@ -1,44 +1,34 @@
 import Controller from '@ember/controller';
-import Ember from 'ember';
+// import $ from 'jquery';
 
 export default Controller.extend({
-  ajax: Ember.inject.service(),
-  actions:{
+  actions: {
     createAccount() {
+      let { email, password /* , firstName, lastName */ } = this.getProperties(['email',  'password', 'firstName', 'lastName']);
 
-      let {
-        email, password, firstName, lastName
-      } = this.getProperties([
-        'email',  'password', 'firstName', 'lastName'
-      ]);
       // email will be the users username
       let username = email;
 
-      let user = {firstName: firstName, lastName:lastName};
+      // let user = { firstName, lastName };
 
-      let newAccount = this.get('store').createRecord('account', {username, password, email});
+      let newAccount = this.get('store').createRecord('account', { username, password, email });
 
-      console.log("TESTRES!");
-
-      newAccount.save({signIn:true}).then((value) =>{
-        console.log(value);
-
-        this.updateUser(value, user);
+      newAccount.save({ signIn: true }).then(() => {
+        // this.updateUser(value, user);
         this.transitionToRoute('/');
-      }).catch(err => {
-        console.log('error' + err);
       });
     }
-  },
-
-  // after account and user creation, update user will form issue
-  updateUser(account, user){
-    return new Promise((resolve, reject) =>{
-      $.ajax( {
-        type : 'PUT',
-        url:'http://localhost:5000/v1/user/' + account._internalModel.id,
-        data: user
-      });
-    });
   }
+  // ,
+  //
+  // // after account and user creation, update user will form issue
+  // updateUser(account, user) {
+  //   return new Promise((resolve, reject) => {
+  //     $.ajax({
+  //       type: 'PUT',
+  //       url: `http://localhost:5000/v1/user/${account._internalModel.id}`,
+  //       data: user
+  //     });
+  //   });
+  // }
 });
