8fd75d2fb271f3513df5a78a61912d4f66abd9d6
Josiah Campbell <jbcampbe@iupui.edu>
2017-12-13 13:30:59 -0500

    getting registration working with the real backend

--- app/adapters/account.js
+++ app/adapters/account.js
@@ -1,7 +1,15 @@
 import AccountAdapter from 'ember-cli-gatekeeper/adapters/account';
+import { computed } from '@ember/object';
 import { isEmpty } from '@ember/utils';
 
 export default AccountAdapter.extend({
+  headers: computed ('gatekeeper.accessToken', function () {
+    let accessToken = this.get ('gatekeeper.accessToken.access_token');
+    let clientToken = this.get ('gatekeeper.client.accessToken.access_token');
+
+    return { Authorization: `Bearer ${accessToken || clientToken}` };
+  }),
+
   urlForQueryRecord(query, modelName) {
     if (isEmpty(Object.keys(query))) {
       return this.buildURL(modelName, 'me', null, 'findRecord', null);
--- app/controllers/register.js
+++ app/controllers/register.js
@@ -1,34 +1,18 @@
 import Controller from '@ember/controller';
-// import $ from 'jquery';
 
 export default Controller.extend({
   actions: {
     createAccount() {
-      let { email, password /* , firstName, lastName */ } = this.getProperties(['email',  'password', 'firstName', 'lastName']);
+      let { email, password } = this.getProperties(['email', 'username', 'password']);
+      let account = this.get('store').createRecord('account', {username: email, password, email});
+      let adapterOptions = {signIn: true};
 
-      // email will be the users username
-      let username = email;
-
-      // let user = { firstName, lastName };
-
-      let newAccount = this.get('store').createRecord('account', { username, password, email });
-
-      newAccount.save({ signIn: true }).then(() => {
-        // this.updateUser(value, user);
+      account.save({adapterOptions}).then(account => {
+        console.log('user created successfully!');
         this.transitionToRoute('/');
+      }).catch(reason => {
+        console.log('error: ' + reason);
       });
     }
   }
-  // ,
-  //
-  // // after account and user creation, update user will form issue
-  // updateUser(account, user) {
-  //   return new Promise((resolve, reject) => {
-  //     $.ajax({
-  //       type: 'PUT',
-  //       url: `http://localhost:5000/v1/user/${account._internalModel.id}`,
-  //       data: user
-  //     });
-  //   });
-  // }
 });
